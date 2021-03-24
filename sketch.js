cf8dd0d4365e6b64cfc742ac365093f51e4f139f
kaashvi-jain <79573608+kaashvi-jain@users.noreply.github.com>
2021-03-24 21:14:45 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var gameState = 0,playerCount;
+var database;
+var form,player,game;
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+   game = new Game();
+   game.getState();
+   game.start();
+
+}
+
+function draw(){
+  
+
+}
